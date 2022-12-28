49d24d54b55b5b167d3efc9a2ad5dd0b985a91a1
ANIK <sinhababuanik@gmail.com>
2022-12-28 23:50:01 +0530

    we have added the add feature to the web app

--- webapp.py
+++ webapp.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from functions import get_todos, write_todos
 
+
+def add_todo():
+    todo_item = st.session_state['new_todo'] + '\n'
+    todo_list.append(todo_item)
+    write_todos(todo_list)
+
+
 st.title("Hello Everyone")
 st.subheader("This is a small web app named todo app list")
 st.write("my self anik and i hope you will like my todo app")
@@ -9,4 +16,6 @@ todo_list = get_todos()
 for todo in todo_list:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo item....")
+st.text_input(label="Hey", placeholder="Add a new todo item....", on_change=add_todo, key='new_todo')
+
+
